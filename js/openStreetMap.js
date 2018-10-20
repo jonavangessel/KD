e5dc24ffd851f22a39747d0d8b9096f9489fdee7
mike <rijndersmike@gmail.com>
2018-10-20 11:58:48 +0200

    hoi

--- js/openStreetMap.js
+++ js/openStreetMap.js
@@ -21,4 +21,204 @@ PolarBear = L.marker([64.74, -69.03]).bindPopup("<h1 class='popup-header'>Polar 
 Lion = L.marker([-20.72, 23.72]).bindPopup("<h1 class='popup-header'>Lion</h1><br/><img src='img/lion.jpeg' width='300px'/>").addTo(map);
 Camel = L.marker([23.16, 7.56]).bindPopup("<h1 class='popup-header'>Camel</h1><br/><img src='img/camel.jpeg' width='300px'/>").addTo(map);
 
-window.alert(hoi);
+$(Koala).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Koala'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(GalapagosTortoise).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Eukaryote . ?animal rdfs:label 'Galápagos tortoise'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(Penguin).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Band . ?animal rdfs:label 'Penguin'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(Bison).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Bison'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(Slut).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Sloth'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+
+$(Panda).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Giant panda'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(MantaRay).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Fish . ?animal rdfs:label 'Manta ray'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(PolarBear).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Polar bear'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(Lion).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Lion'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
+$(Camel).click(function () {
+    let mysparqlendpoint = "http://dbpedia.org/sparql?query=";
+    let mysparqlquery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbo: <http://dbpedia.org/ontology/> SELECT DISTINCT ?animal ?comment WHERE { ?animal rdf:type dbo:Mammal . ?animal rdfs:label 'Camel'@en . ?animal dbo:abstract ?comment FILTER (lang(?comment) = 'en') }";
+    let queryURL = mysparqlendpoint+encodeURI(mysparqlquery).replace(/#/g, '%23');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: queryURL,
+        success: function (data) {
+            let content = JSON.stringify(data);
+            document.getElementById("col-comment").innerHTML = content;
+        }
+        // let content = data.results.bindings.0.comment.value;
+        // console.log(content);
+        // let obj = JSON.parse(data);
+        // let result = obj.comment;
+        //
+    })
+});
+
